8c943fe9ebe8ab5c19e9242987da85ae7ef03e84
education-bootcamp <education.seekerscloud@gmail.com>
2023-11-10 21:55:04 +0530

    2

--- src/main/java/com/devstack/customerserviceapi/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/devstack/customerserviceapi/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,30 @@
 package com.devstack.customerserviceapi.service.impl;
+import com.devstack.customerserviceapi.entity.Customer;
 
 import com.devstack.customerserviceapi.dto.CustomerDto;
+import com.devstack.customerserviceapi.repo.CustomerRepo;
 import com.devstack.customerserviceapi.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
     @Override
     public void createCustomer(CustomerDto dto) {
+        Customer customer = new Customer(
+                dto.getId(), dto.getName(), dto.getAddress(), dto.getSalary()
+        );
 
+        customerRepo.save(customer);
     }
 }
