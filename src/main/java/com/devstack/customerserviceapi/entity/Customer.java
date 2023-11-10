f32033f1fdeb34066098c92ea955f2bc4b2e5cc3
education-bootcamp <education.seekerscloud@gmail.com>
2023-11-10 21:25:50 +0530

    Initial commit

---
+++ src/main/java/com/devstack/customerserviceapi/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.devstack.customerserviceapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    @Id
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "address", nullable = false)
+    private String address;
+    @Column(name = "salary", nullable = false)
+    private double salary;
+}
